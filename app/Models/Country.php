99f1e48d2761ed122d586121fe592847bd5396ff
zaeem9233 <zaeemali91@gmail.com>
2024-03-06 23:17:24 +0500

    Country Model has been created

---
+++ app/Models/Country.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+    use HasFactory;
+
+    protected $table = 'countries';
+
+    protected $fillable = [
+        'id',
+        'name',
+        'iso3',
+        'numeric_code',
+        'iso2',
+        'phonecode',
+        'capital',
+        'currency',
+        'currency_name',
+        'currency_symbol',
+        'tld',
+        'native',
+        'region',
+        'region_id',
+        'subregion',
+        'subregion_id',
+        'nationality',
+        'timezones',
+        'translations',
+        'latitude',
+        'longitude',
+        'emoji',
+        'emojiU',
+        'created_at',
+        'updated_at',
+        'flag',
+        'wikiDataId'
+    ];
+}
